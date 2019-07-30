a3fa0cb64f4743acc185da18e6973150b13d95e8
Erick Leon <eeleon@edu.uwaterloo.ca>
2019-07-29 21:56:24 -0400

    project section wip

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import Section from 'components/Section'
+import { DIMENSIONS, COLORS } from 'App/AppConstants'
+import styled from 'styled-components'
 
 class Projects extends React.Component<any> {
 	public render() {
-		return <Section title="Projects" />
+		return <Section title="Projects"></Section>
 	}
 }
 
